0d6c0e0d98377b45b676aa040b3680294561e66d
Roman Gushchik <adrianed.fabled@gmail.com>
2022-06-20 16:48:44 +0300

    add new request, add new task for request, add objects for Weather

--- HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/ViewController.swift
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/ViewController.swift
@@ -11,9 +11,33 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        if let url = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=Minsk&appid=2fbe78d61a988be7934bebfe2d3468ce") {
+            var urlRequest = URLRequest(url: url)
+            // тип запроса
+            urlRequest.httpMethod = "GET"
+            // добавляем в заголовок тип контента
+            urlRequest.setValue("application/json", forHTTPHeaderField: "Content-Type")
+            // создаем таск для непосредственного запроса на сервер
+            let dataTask = URLSession.shared.dataTask(with: urlRequest) { data, response, error  in
+                if let response = response {
+                    print(response)
+                }
+                if let data = data {
+                    let users = try! JSONDecoder().decode(WelcomeToWeather.self, from: data)
+                    print(users)
+                    print(data)
+               }
+                if let error = error {
+                    print(error)
+                }
+            }
+            // запускаем запрос на выполнение
+            dataTask.resume()
+        }
     }
-
-
 }
+
+
+
 
