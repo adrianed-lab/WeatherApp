fbe2b9fa61a10ccf4ec3276d69693c1191250990
Roman Gushchik <adrianed.fabled@gmail.com>
2022-06-20 13:33:41 +0300

    add new project to repository

---
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HomeWork24_RomanGushchik
+//
+//  Created by admin on 20.06.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
