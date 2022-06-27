e6b5d082a6fe607ea5eaf0569623c66428e55db4
Roman Gushchik <adrianed.fabled@gmail.com>
2022-06-27 09:46:45 +0300

    add object "map"

--- HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/ViewControllers/MapViewController.swift
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/ViewControllers/MapViewController.swift
@@ -6,17 +6,34 @@
 //
 
 import UIKit
+import GoogleMaps
 
 class MapViewController: UIViewController {
+    @IBOutlet weak var conteinerForMapView: UIView!
     static let key = "MapViewController"
 
     override func viewDidLoad() {
         super.viewDidLoad()
+       // view.layoutSubviews()
         title = "Map"
+        let camera = GMSCameraPosition.camera(withLatitude: 54.029, longitude: 27.597, zoom: 6.0)
+        let mapView = GMSMapView.map(withFrame: conteinerForMapView.frame, camera: camera)
+        conteinerForMapView.addSubview(mapView)
+       view.addSubview(conteinerForMapView)
+       // view.addSubview(mapView)
+
+        mapView.delegate = self
+
+        
         
     }
     
 
    
 
+}
+extension MapViewController: GMSMapViewDelegate {
+    func mapView(_ mapView: GMSMapView, didTapAt coordinate: CLLocationCoordinate2D) {
+        
+    }
 }
