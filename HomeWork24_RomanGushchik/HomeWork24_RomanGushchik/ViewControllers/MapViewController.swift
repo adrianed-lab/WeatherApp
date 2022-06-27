8a2e93109df1e23c35a6d078ef7b89ac2b4552e9
Roman Gushchik <adrianed.fabled@gmail.com>
2022-06-27 08:38:04 +0300

    add class "MapViewController, TabBarViewController"

---
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/ViewControllers/MapViewController.swift
@@ -0,0 +1,22 @@
+//
+//  MapViewController.swift
+//  HomeWork24_RomanGushchik
+//
+//  Created by admin on 27.06.22.
+//
+
+import UIKit
+
+class MapViewController: UIViewController {
+    static let key = "MapViewController"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        title = "Map"
+        
+    }
+    
+
+   
+
+}
