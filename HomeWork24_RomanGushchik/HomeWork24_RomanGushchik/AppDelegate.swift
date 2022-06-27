944c42f109fcedfeff574d15e8856c1180a685e4
Roman Gushchik <adrianed.fabled@gmail.com>
2022-06-27 09:46:10 +0300

    update MapStoryboard, add key for googleMaps

--- HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/AppDelegate.swift
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/AppDelegate.swift
@@ -6,6 +6,7 @@
 //
 
 import UIKit
+import GoogleMaps
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
@@ -13,7 +14,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let valueApiKey = Bundle.main.infoDictionary?["API_KEY_GoogleMaps"] as? String
+        if let valueApiKey = valueApiKey {GMSServices.provideAPIKey(valueApiKey)}
         return true
     }
 
