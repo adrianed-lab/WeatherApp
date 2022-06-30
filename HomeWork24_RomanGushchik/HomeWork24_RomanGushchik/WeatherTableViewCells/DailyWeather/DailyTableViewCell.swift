7f7dcbcec7d804ec08ca890a0af3d10d7d9b87a3
Roman Gushchik <adrianed.fabled@gmail.com>
2022-06-30 12:09:37 +0300

    add DailyTableViewCell, add HourlyTableViewCell

---
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/WeatherTableViewCells/DailyWeather/DailyTableViewCell.swift
@@ -0,0 +1,23 @@
+//
+//  DailyTableViewCell.swift
+//  HomeWork24_RomanGushchik
+//
+//  Created by admin on 30.06.22.
+//
+
+import UIKit
+
+class DailyTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
