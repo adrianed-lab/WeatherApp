63ecc5cd0c0498d7944306e7a386b4387ddd2fd4
Roman Gushchik <adrianed.fabled@gmail.com>
2022-06-30 12:10:46 +0300

    add method "configure" for DailyTableViewCell

--- HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/WeatherTableViewCells/DailyWeather/DailyTableViewCell.swift
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/WeatherTableViewCells/DailyWeather/DailyTableViewCell.swift
@@ -8,16 +8,42 @@
 import UIKit
 
 class DailyTableViewCell: UITableViewCell {
+    @IBOutlet var dayLabel: UILabel!
+    @IBOutlet var lowTempLabel: UILabel!
+    @IBOutlet var hightTempLebl: UILabel!
+    @IBOutlet var iconWeatherImage: UIImageView!
 
+    
+    static let key = "DailyTableViewCell"
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
-        // Configure the view for the selected state
     }
+    
+    func configure(model: Daily) {
+        self.lowTempLabel.text = "\(model.temperature.min)°"
+        self.hightTempLebl.text = "\(model.temperature.max)°"
+        self.dayLabel.text = getDayForDate(Date(timeIntervalSince1970: Double(model.dateTime)))
+        guard let iconImage = model.weather.first?.icon else {return}
+        guard let imageUrl = URL(string: "https://openweathermap.org/img/wn/\(iconImage)@2x.png") else {return}
+        if let data = try? Data(contentsOf: imageUrl) {
+            self.iconWeatherImage.image = UIImage(data: data)
+        }
+    }
+    func getDayForDate(_ date: Date?) -> String {
+        guard let inputDate = date else {
+            return ""
+        }
+        
+        let formatter = DateFormatter()
+        formatter.dateFormat = "d MMM, yyyy" // Monday
+        return formatter.string(from: inputDate)
+    }
+    
     
 }
