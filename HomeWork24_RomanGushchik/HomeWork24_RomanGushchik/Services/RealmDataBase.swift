c1df023f6d85e2aa5fe0e871eddf9c90419615fe
Roman Gushchik <adrianed.fabled@gmail.com>
2022-07-05 21:26:07 +0300

    update service RealmDataBase

--- HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/Services/RealmDataBase.swift
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/Services/RealmDataBase.swift
@@ -9,23 +9,23 @@ import Foundation
 import RealmSwift
 import UIKit
 
-protocol RealmDataBase {
+protocol RealmDataBaseProtocol {
     func getDataBase(value: CurrentWeather)
 }
 
-class CurrentRealmDataBase: RealmDataBase {
+class RealmDataBase: RealmDataBaseProtocol {
     func getDataBase(value: CurrentWeather) {
             guard let weather = value.current.weather.first?.weatherDescription else {return}
             let date = Date()
             let realm = try! Realm()
             try! realm.write {
-                let realmCurrentWeatherDataBase = RealmCurrentWeatherDataBase()
+                let realmCurrentWeatherDataBase = CurrentWeatherData()
                 realmCurrentWeatherDataBase.temp = value.current.temperature
                 realmCurrentWeatherDataBase.feelsLike = value.current.feelsLike
                 realmCurrentWeatherDataBase.weatherDiscription = weather
                 realmCurrentWeatherDataBase.time = Int(date.timeIntervalSince1970)
                 realm.add(realmCurrentWeatherDataBase)
-                let realmDataBase = RealmDataBase()
+                let realmDataBase = CurrentPlaceData()
                 realmDataBase.lat = value.latitude
                 realmDataBase.lon = value.longitude
                 realmDataBase.time = Int(date.timeIntervalSince1970)
