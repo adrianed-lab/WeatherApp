4147639b2de7158c7dda8754b7fc9444f66df7ff
Roman Gushchik <adrianed.fabled@gmail.com>
2022-07-11 02:29:04 +0300

    add enum for mainWeather

--- HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/Models/HourlyWeather.swift
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/Models/HourlyWeather.swift
@@ -35,7 +35,7 @@ struct Hourly: Codable {
 
 struct WeatherForHours: Codable {
     let id: Int
-    let main: String
+    let main: Main
     let weatherDescription: String
     let icon: String
 
@@ -45,6 +45,26 @@ struct WeatherForHours: Codable {
         case icon
     }
 }
+enum Main: String, Codable {
+        case rain = "Rain"
+        case snow = "Snow"
+        case thunderstorm = "Thunderstorm"
+        case clouds = "Clouds"
+        case clear = "Clear"
+        case mist = "Mist"
+        case smoke = "Smoke"
+        case haze = "Haze"
+        case dust = "Dust"
+        case fog = "Fog"
+        case sand = "Sand"
+        case ash = "Ash"
+        case squall = "Squall"
+        case tornado = "Tornado"
+    }
+
+
+
+
 
 
 
