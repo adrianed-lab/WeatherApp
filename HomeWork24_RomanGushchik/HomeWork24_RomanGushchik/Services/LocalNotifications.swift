48003f951720dc6e143cacd4a4cb1576369cfd3c
Roman Gushchik <adrianed.fabled@gmail.com>
2022-07-11 02:30:41 +0300

    add file LocalNotifications

---
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/Services/LocalNotifications.swift
@@ -0,0 +1,52 @@
+//
+//  LocalNotification.swift
+//  HomeWork24_RomanGushchik
+//
+//  Created by admin on 10.07.22.
+//
+
+import Foundation
+import UIKit
+import UserNotifications
+
+protocol NotificationProtocol {
+    func createLocalNotification(valueWeather: [Hourly])
+    func removeNotification(withIdentifiers identifiers: [String])
+}
+class UserNotification: NotificationProtocol {
+    func removeNotification(withIdentifiers identifiers: [String]) {
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.removePendingNotificationRequests(withIdentifiers: identifiers)
+        
+    }
+    
+    func createLocalNotification(valueWeather: [Hourly]) {
+        removeNotification(withIdentifiers: ["Check notification"])
+        guard let mainWeather = valueWeather.first?.weather.first?.main else {return}
+        let notificationCenter = UNUserNotificationCenter.current()
+        notificationCenter.requestAuthorization(options: [.alert, .badge, .sound]) { permissionGranted, error in
+            if !permissionGranted {
+                    print("Permission Denied")
+            }
+        }
+        notificationCenter.getNotificationSettings { settings in
+            if settings.authorizationStatus == .authorized {
+                if mainWeather == .rain || mainWeather == .thunderstorm || mainWeather == .snow {
+                let content = UNMutableNotificationContent()
+                content.title = "Weather Local Notification"
+                content.body = "Check weather"
+                content.sound = UNNotificationSound.default
+                let identifier = "Check notification"
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1800, repeats: false)
+                let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+                notificationCenter.add(request) { error in
+                    print(error?.localizedDescription)
+                        }
+                    }
+                }
+            }
+        }
+    }
+    
+    
+
