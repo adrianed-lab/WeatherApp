f06a2bad9e895694753a290d05e25e0387c9a712
Roman Gushchik <adrianed.fabled@gmail.com>
2022-07-14 00:09:28 +0300

    add viewHeaders for sections

--- HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/Extensions/WeatherViewController+Extension.swift
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/Extensions/WeatherViewController+Extension.swift
@@ -17,7 +17,7 @@ extension WeatherViewController: UITableViewDelegate, UITableViewDataSource {
         if section == 0 {
             return 1
         }
-         return models.count
+         return dailyWeather.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
@@ -28,12 +28,51 @@ extension WeatherViewController: UITableViewDelegate, UITableViewDataSource {
         }
         else {
         let cell = tableView.dequeueReusableCell(withIdentifier: DailyTableViewCell.key, for: indexPath) as! DailyTableViewCell
-            cell.configure(model: models[indexPath.row])
+            if indexPath.row == 0 {
+                cell.configureForToDay(model: dailyWeather[indexPath.row])
+                return cell
+            }
+            cell.configure(model: dailyWeather[indexPath.row])
             return cell
         }
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        150
+        if indexPath.section == 0 {
+            return 100
+        }
+        return 60
+    }
+    
+        func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        30
+    }
+    
+    func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        if section == 0 {
+            let header = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 30))
+            header.backgroundColor = .clear
+            let imageView = UIImageView(image: UIImage(systemName: "clock"))
+            imageView.tintColor = .orange
+            header.addSubview(imageView)
+            imageView.frame = CGRect(x: 5, y: 5, width: header.frame.size.height - 10, height: header.frame.size.height - 10)
+            let label = UILabel(frame: CGRect(x: 10 + imageView.frame.size.width, y: 5, width: header.frame.size.width - 15 - imageView.frame.size.width, height: header.frame.size.height - 10))
+            header.addSubview(label)
+            label.textColor = .orange
+            label.text = "HOURLY FORECAST"
+            return header
+        } else {
+            let header = UIView(frame: CGRect(x: 0, y: 0, width: view.frame.size.width, height: 30))
+            header.backgroundColor = .clear
+            let imageView = UIImageView(image: UIImage(systemName: "calendar"))
+            imageView.tintColor = .orange
+            header.addSubview(imageView)
+            imageView.frame = CGRect(x: 5, y: 5, width: header.frame.size.height - 10, height: header.frame.size.height - 10)
+            let label = UILabel(frame: CGRect(x: 10 + imageView.frame.size.width, y: 5, width: header.frame.size.width - 15 - imageView.frame.size.width, height: header.frame.size.height - 10))
+            header.addSubview(label)
+            label.textColor = .orange
+            label.text = "8-DAY FORECAST"
+            return header
+        }
     }
 }
