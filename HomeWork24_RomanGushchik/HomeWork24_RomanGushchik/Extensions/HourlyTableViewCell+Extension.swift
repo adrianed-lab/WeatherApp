046f6e120383a3b48838f0be3ef84591ac31cb71
Roman Gushchik <adrianed.fabled@gmail.com>
2022-07-28 15:53:48 +0300

    update size for collection cell

--- HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/Extensions/HourlyTableViewCell+Extension.swift
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/Extensions/HourlyTableViewCell+Extension.swift
@@ -11,7 +11,7 @@ import UIKit
 extension HourlyTableViewCell: UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout {
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: 60, height: 100)
+        return CGSize(width: indexPath.row != 0 ? 60 : 80, height: 100)
    }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
