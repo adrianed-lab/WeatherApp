82b23a4b0fb6578008b9affdd641bb109560fee5
Roman Gushchik <adrianed.fabled@gmail.com>
2022-07-28 15:57:38 +0300

    add flagState label , update localization

--- HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/TableViewCells/WeatherTableViewCells/HistoryRequest/HistoryRequestTableViewCell.swift
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/TableViewCells/WeatherTableViewCells/HistoryRequest/HistoryRequestTableViewCell.swift
@@ -15,6 +15,7 @@ class HistoryRequestTableViewCell: UITableViewCell {
     @IBOutlet weak var temperatureLabel: UILabel!
     @IBOutlet weak var feelsLikeLabel: UILabel!
     @IBOutlet weak var dateTimeLabel: UILabel!
+    @IBOutlet weak var flagState: UILabel!
     @IBOutlet weak var myStackView: UIStackView!
     static let key = "HistoryRequestTableViewCell"
     override func awakeFromNib() {
@@ -28,11 +29,12 @@ class HistoryRequestTableViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     func configure(model: CurrentPlaceData) {
-        self.latitudeLabel.text = "Lat:".localizable(key: "Latitude") + "\(model.lat)"
-        self.longitudeLabel.text = "Lon:".localizable(key: "Longitude") + "\(model.lon)"
+        self.latitudeLabel.text = String(format: NSLocalizedString("Latitude", comment: ""), Int(model.lat))
+        self.longitudeLabel.text = String(format: NSLocalizedString("Longitude", comment: ""), Int(model.lon))
         self.weatherLabel.text = model.currentWeather.weatherDiscription
-        self.temperatureLabel.text = "Temp:".localizable(key: "Temp") + "\(model.currentWeather.temp)"
-        self.feelsLikeLabel.text = "FeelsLike:".localizable(key: "FeelsLike") + "\(model.currentWeather.feelsLike)"
+        self.temperatureLabel.text = String(format: NSLocalizedString("Temp", comment: ""), Int(model.currentWeather.temp))
+        self.feelsLikeLabel.text = String(format: NSLocalizedString("FeelsLike", comment: ""), Int(model.currentWeather.feelsLike))
         self.dateTimeLabel.text = model.currentWeather.dateTime.timeIntervalToStringDate(.fullDate)
+        self.flagState.text = String(format: NSLocalizedString("FlagState", comment: ""), model.requestFlag)
     }
 }
