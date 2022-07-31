dce9005fa6a7256323df9d5c709b9e6edc840a6c
Roman Gushchik <adrianed.fabled@gmail.com>
2022-07-31 16:54:17 +0300

    add Extension for SettingsViewController

---
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/Extensions/SettingsViewControllert+Extension.swift
@@ -0,0 +1,24 @@
+//
+//  SettingsViewControllert+Extension.swift
+//  HomeWork24_RomanGushchik
+//
+//  Created by admin on 31.07.22.
+//
+
+import Foundation
+import UIKit
+
+
+extension SettingsViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        2
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        <#code#>
+    }
+    
+    
+    
+    
+}
