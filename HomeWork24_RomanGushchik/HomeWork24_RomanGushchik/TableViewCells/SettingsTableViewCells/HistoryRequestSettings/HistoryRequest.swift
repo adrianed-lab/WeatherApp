f108d381e23cdf1661bf5db31d2e52bdf2335448
Roman Gushchik <adrianed.fabled@gmail.com>
2022-07-31 16:55:56 +0300

    add AdverseWeather, HistoryRequest TableViewCells

---
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/TableViewCells/SettingsTableViewCells/HistoryRequestSettings/HistoryRequest.swift
@@ -0,0 +1,21 @@
+//
+//  HistoryRequest.swift
+//  HomeWork24_RomanGushchik
+//
+//  Created by admin on 31.07.22.
+//
+
+import UIKit
+
+class HistoryRequest: UITableViewCell {
+    static let key = "HistoryRequest"
+    override func awakeFromNib() {
+        super.awakeFromNib()
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+        
+    }
+    
+}
