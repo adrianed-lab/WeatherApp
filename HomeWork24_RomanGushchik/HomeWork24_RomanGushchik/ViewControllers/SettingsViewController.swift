069d88c6bae256864032e96abbca1aaacc904424
Roman Gushchik <adrianed.fabled@gmail.com>
2022-08-08 19:05:37 +0300

    update SettingsViewController

--- HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/ViewControllers/SettingsViewController.swift
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/ViewControllers/SettingsViewController.swift
@@ -6,19 +6,23 @@
 //
 
 import UIKit
+import RealmSwift
 
 class SettingsViewController: UIViewController {
     @IBOutlet weak var settingsTableView: UITableView!
     static let key = "SettingsViewController"
+    var valueBadWeather: Results<RealmBadWeather>!
+    var realmProtocol: RealmDataBaseProtocol!
     
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         title = "SettingsTabBarItem".localizable()
-        settingsTableView.register(UINib(nibName: "AdverseWeatherConditions", bundle: nil), forCellReuseIdentifier: AdverseWeatherConditions.key)
-    
+        settingsTableView.register(UINib(nibName: "BadWeatherConditionsTableViewCell", bundle: nil), forCellReuseIdentifier: BadWeatherConditionsTableViewCell.key )
+        settingsTableView.register(UINib(nibName: "RequestTableViewCell", bundle: nil), forCellReuseIdentifier: RequestTableViewCell.key)
+        realmProtocol = RealmDataBase()
+        valueBadWeather = realmProtocol.getBadWeather(nameObject: RealmBadWeather.self)
+        
         }
-    
-
     }
