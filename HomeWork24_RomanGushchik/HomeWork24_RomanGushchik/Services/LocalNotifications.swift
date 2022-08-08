3426c2f8e310fca7ea9da26efca380e0a11490ec
Roman Gushchik <adrianed.fabled@gmail.com>
2022-08-08 19:01:20 +0300

    add method "addNotification", update method "createLocalNotification"

--- HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/Services/LocalNotifications.swift
+++ HomeWork24_RomanGushchik/HomeWork24_RomanGushchik/Services/LocalNotifications.swift
@@ -10,8 +10,9 @@ import UIKit
 import UserNotifications
 
 protocol NotificationProtocol {
-    func createLocalNotification(valueWeather: [Hourly])
+    func createLocalNotification(valueWeather: [Hourly], badWeather: BadWeather)
     func removeNotification(withIdentifiers identifiers: [String])
+    func addNotification()
 }
 class UserNotification: NotificationProtocol {
     let notificationCenter = UNUserNotificationCenter.current()
@@ -20,7 +21,7 @@ class UserNotification: NotificationProtocol {
         notificationCenter.removePendingNotificationRequests(withIdentifiers: identifiers)
     }
     
-    func createLocalNotification(valueWeather: [Hourly]) {
+    func createLocalNotification(valueWeather: [Hourly], badWeather: BadWeather) {
         removeNotification(withIdentifiers: ["Check notification"])
         guard let mainWeather = valueWeather.first?.weather.first?.main else {return}
         notificationCenter.requestAuthorization(options: [.alert, .badge, .sound]) { permissionGranted, error in
@@ -29,21 +30,31 @@ class UserNotification: NotificationProtocol {
             }
         }
         notificationCenter.getNotificationSettings { settings in
-            if settings.authorizationStatus == .authorized && (mainWeather == .rain || mainWeather == .thunderstorm || mainWeather == .snow) {
-                let content = UNMutableNotificationContent()
-                    content.title = "WeatherNotification".localizable()
-                    content.body = "Check weather".localizable()
-                content.sound = UNNotificationSound.default
-                let identifier = "Check notification"
-                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 1800, repeats: false)
-                let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
-                    self.notificationCenter.add(request) { error in
-                        guard let error = error else {return}
-                    print(error.localizedDescription)
-                        }
-                    }
+            if settings.authorizationStatus == .authorized {
+                if badWeather.contains(.rain) && mainWeather == .rain {
+                    self.addNotification()
+                } else if badWeather.contains(.snow) && mainWeather == .snow {
+                    self.addNotification()
+                } else if badWeather.contains(.thunderstorm) && mainWeather == .thunderstorm {
+                    self.addNotification()
                 }
             }
+        }
+    }
+    
+    func addNotification() {
+        let content = UNMutableNotificationContent()
+        content.title = "WeatherNotification".localizable()
+        content.body = "Check weather".localizable()
+        content.sound = UNNotificationSound.default
+        let identifier = "Check notification"
+        let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+        let request = UNNotificationRequest(identifier: identifier, content: content, trigger: trigger)
+        self.notificationCenter.add(request) { error in
+            guard let error = error else {return}
+        print(error.localizedDescription)
+            }
+        }
     }
     
     
